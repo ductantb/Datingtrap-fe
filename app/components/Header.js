2012b0e7f32afe7b2a34775839e8dc5c46c83d4e
QuangVu04 <quangvu1782004@gmail.com>
2025-05-22 21:52:58 +0700

    notiscreen

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,6 +4,7 @@ import React from "react";
 import { useNavigation } from "@react-navigation/native";
 
 const Header = () => {
+  const unreadCount = 5;
   const navigation = useNavigation();
   return (
     <View className="flex-row justify-between items-center px-5">
@@ -23,6 +24,17 @@ const Header = () => {
       <TouchableOpacity onPress={() => navigation.navigate("ChatList")}>
         <Ionicons name="chatbubbles-outline" size={30} color="black" />
       </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate("Notifications")}>
+      <View>
+        <Ionicons name="notifications-outline" size={30} color="black" />
+          {unreadCount > 0 && (
+      <View className="absolute -top-2 -right-2 bg-red-500 rounded-full w-5 h-5 items-center justify-center">
+        <Text className="text-white text-xs">{unreadCount}</Text>
+      </View>
+    )}
+  </View>
+  </TouchableOpacity>
     </View>
   );
 };

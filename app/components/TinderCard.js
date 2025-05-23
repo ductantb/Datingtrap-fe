0db2b67d91f1173c07dbf61ab1858eb4bc08697e
thaidzuicui <hthai313204@gmail.com>
2025-05-23 08:51:31 +0700

    swipe card ui done

--- app/components/TinderCard.js
+++ app/components/TinderCard.js
@@ -97,72 +97,73 @@ const TinderCardComponent = () => {
   };
 
   return (
-    <View className="flex-1 px-5 pt-5 ">
-      {db.map((character, index) => (
-        <TinderCard
-          ref={childRefs[index]}
-          key={character.id}
-          onSwipe={(dir) => swiped(dir, character.name, index)}
-          onCardLeftScreen={() => outOfFrame(character.name, index)}
-          preventSwipe={["up", "down"]}
-          flickOnSwipe={true}
-          swipeRequirementType="position"
-          swipeThreshold={width * 0.65}
-        >
-          <View className="absolute">
-            <TouchableWithoutFeedback>
-              <Image
-                source={{ uri: character.avatarUrl }}
+    <View className="flex h-screen">
+      <View className="flex-1 px-5 pt-5 ">
+        {db.map((character, index) => (
+          <TinderCard
+            ref={childRefs[index]}
+            key={character.id}
+            onSwipe={(dir) => swiped(dir, character.name, index)}
+            onCardLeftScreen={() => outOfFrame(character.name, index)}
+            preventSwipe={["up", "down"]}
+            flickOnSwipe={true}
+            swipeRequirementType="position"
+            swipeThreshold={width * 0.65}
+          >
+            <View className="absolute">
+              <TouchableWithoutFeedback>
+                <Image
+                  source={{ uri: character.avatarUrl }}
+                  style={{
+                    width: width * 0.91,
+                    height: height * 0.75,
+                  }}
+                  resizeMode="cover"
+                  className="rounded-3xl"
+                />
+              </TouchableWithoutFeedback>
+
+              <LinearGradient
+                colors={["transparent", "rgba(0,0,0,0.9)"]}
                 style={{
-                  width: width * 0.91,
-                  height: height * 0.75,
+                  position: "absolute",
+                  bottom: 0,
+                  width: "100%",
+                  height: "100%",
+                  borderBottomLeftRadius: 24,
+                  borderBottomRightRadius: 24,
                 }}
-                resizeMode="cover"
-                className="rounded-3xl"
+                start={{ x: 0.5, y: 0.5 }}
+                end={{ x: 0.5, y: 1 }}
               />
-            </TouchableWithoutFeedback>
 
-            <LinearGradient
-              colors={["transparent", "rgba(0,0,0,0.9)"]}
-              style={{
-                position: "absolute",
-                bottom: 0,
-                width: "100%",
-                height: "100%",
-                borderBottomLeftRadius: 24,
-                borderBottomRightRadius: 24,
-              }}
-              start={{ x: 0.5, y: 0.5 }}
-              end={{ x: 0.5, y: 1 }}
-            />
+              <View className="absolute bottom-10 justify-start w-full items-start pl-4">
+                <View className="flex-row justify-center items-center ">
+                  <Text className="text-2xl text-white font-bold">
+                    {character.name}
+                    {", "}
+                  </Text>
+                  <Text className="text-2xl text-white font-bold mr-2">
+                    {character.age}
+                  </Text>
+                </View>
 
-            <View className="absolute bottom-10 justify-start w-full items-start pl-4">
-              <View className="flex-row justify-center items-center ">
-                <Text className="text-2xl text-white font-bold">
-                  {character.name}
-                  {", "}
-                </Text>
-                <Text className="text-2xl text-white font-bold mr-2">
-                  {character.age}
-                </Text>
-              </View>
-
-              {/* Location */}
-              <View className="flex-row justify-center items-center ">
-                <Text className="text-lg text-white font-regular">
-                  Hanoi
-                  {", "}
-                </Text>
-                <Text className="text-lg text-white font-regular mr-2">
-                  Viet Nam
-                </Text>
+                {/* Location */}
+                <View className="flex-row justify-center items-center ">
+                  <Text className="text-lg text-white font-regular">
+                    Hanoi
+                    {", "}
+                  </Text>
+                  <Text className="text-lg text-white font-regular mr-2">
+                    Viet Nam
+                  </Text>
+                </View>
               </View>
             </View>
-          </View>
-        </TinderCard>
-      ))}
-
-      <View className="flex-row justify-around items-end mt-10 px-8 ">
+          </TinderCard>
+        ))}
+      </View>
+      <View className="flex-row justify-around items-end mb-24 px-8">
         <TouchableWithoutFeedback onPress={() => swipe("left")}>
           <View className="w-20 h-20 bg-red-300 rounded-full justify-center items-center shadow-md">
             <AntDesign name="dislike1" size={30} color="red" />
